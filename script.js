7a7402d943616212662717f288f392cffc6158c2
IMTheBale <johnny.bale.play@gmail.com>
2023-06-08 17:18:40 +0300

    changes

---
+++ script.js
@@ -0,0 +1,11 @@
+const averageSpeed = 57;
+const mpg = 27;
+const gallons = 13;
+const destination = "Ithaca, NY";
+const tripMiles = 74 + 63 + 167;
+const time = tripMiles / averageSpeed;
+const tankRange = mpg * gallons;
+const leftoverGasRange = tankRange - tripMiles;
+console.log("Trip to " + destination);
+console.log("Estimated Time: " + time);
+console.log("Range Remaining: " + leftoverGasRange);
